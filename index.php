299ce6bcf580eaea64873a38ebf8fc59d4e3572a
Anderson Souza <andersonsouza007@live.com>
2022-11-25 11:08:45 -0300

    Alteração visual da escolha do horário

--- index.php
+++ index.php
@@ -27,62 +27,62 @@
         
             <label for="oito" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="oito" name="HoraPresenca[]" value="08:00">
-                 08:00 
+                 08h às 09h 
             </label>
 
             <label for="nove" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="nove" name="HoraPresenca[]" value="09:00">
-                09:00
+                09h às 10h
             </label>
 
             <label for="dez" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="dez" name="HoraPresenca[]" value="10:00">
-                10:00
+                10h às 11h
             </label>
 
             <label for="onze" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="onze" name="HoraPresenca[]" value="11:00">
-                11:00
+                11h às 12h
             </label>
 
             <label for="doze" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="doze" name="HoraPresenca[]" value="12:00">
-                12:00
+                12h às 13h
             </label>
 
             <label for="treze" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="treze" name="HoraPresenca[]" value="13:00">
-                13:00
+                13h às 14h
             </label>
 
             <label for="catorze" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="catorze" name="HoraPresenca[]" value="14:00">
-                14:00
+                14h às 15h
             </label>
 
             <label for="quinze" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="quinze" name="HoraPresenca[]" value="15:00">
-                15:00
+                15h às 16h
             </label>
 
             <label for="dezesseis" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="dezesseis" name="HoraPresenca[]" value="16:00">
-                16:00
+                16h às 17h
             </label>
 
             <label for="dezessete" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="dezessete" name="HoraPresenca[]" value="17:00">
-                17:00
+                17h às 18h
             </label>
 
             <label for="dezoito" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="dezoito" name="HoraPresenca[]" value="18:00">
-                18:00
+                18h às 19h
             </label>
             
             <label for="dezenove" class="form-check-label text-white fs-5">
                 <input type="checkbox" class="form-check-input mx-2" id="dezenove" name="HoraPresenca[]" value="19:00">
-                19:00
+                19h às 20h
             </label>
         </div>
         <div class="data-hora m-1">
